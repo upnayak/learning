ba08ff8ab8ad3ffa8f9d76783dda0f44e41ccbe1
upnayak <70261281+upnayak@users.noreply.github.com>
2020-10-21 15:28:16 +0530

    Create countUniqueValues.js

---
+++ data_structures/countUniqueValues.js
@@ -0,0 +1,17 @@
+function countUniqueValues(arr){
+    if (arr.length===0){
+        return 0
+    }
+    let i = 0
+    let j = 1;
+    while(j < arr.length){
+        console.log(arr);
+        if(arr[i]!=arr[j])
+            i++;
+            arr[i] = arr[j]
+        j++;
+    }
+    return i+1
+}
+
+console.log(countUniqueValues([]))
